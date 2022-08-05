3c4103991d54dae5a80d2482ca2503c59d405229
miya-ryu <83804046+miya-ryu@users.noreply.github.com>
2022-08-05 13:40:41 +0900

    初期設定

---
+++ 2022_Pac-Man_D/Input.h
@@ -0,0 +1,8 @@
+#pragma once
+
+extern int iOldKey; //前回の入力キー
+extern int iNowKey; //今回の入力キー
+extern int iKeyFlg; //入力キー情報
+
+//キー入力取得関数
+void InputKey(void);
--- 2022_Pac-Man_D/main.cpp
+++ 2022_Pac-Man_D/main.cpp
@@ -1,17 +1,20 @@
 #include "DxLib.h"
+#include "Input.h"
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-{
-	if (DxLib_Init() == -1)	
-	{
-		return -1;
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	SetMainWindowText("Drive_and_Avoid");   //タイトルを設定
+	ChangeWindowMode(TRUE);             //ウィンドウモード変更
+	SetGraphMode(1280, 720, 32);
+	DxLib_Init();                       // DXライブラリ初期化
+	SetDrawScreen(DX_SCREEN_BACK);      //裏画面設定
+
+	while (ProcessMessage() == 0) { //メッセージ処理
+		ClearDrawScreen(); //画面消去
+
+		InputKey();        //入力
+
+		ScreenFlip();      //画面更新
 	}
-
-	DrawPixel(320, 240, GetColor(255, 255, 255));
-
-	WaitKey();
-
-	DxLib_End();
-
-	return 0;
+	DxLib_End(); // DXライブラリ終了処理
+	//return 0;
 }
